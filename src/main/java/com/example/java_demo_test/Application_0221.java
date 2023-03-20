ffe8660c5466f240b29c00b6c2d2b657cc37d891
liang999999 <a20060326@yahoo.com.tw>
2023-03-20 11:50:51 +0800

    0221 commit

---
+++ src/main/java/com/example/java_demo_test/Application_0221.java
@@ -0,0 +1,14 @@
+package com.example.demo_test;
+
+public class Application_0221 {
+
+	public static void main(String[] args) {
+		int x =120;
+		int z = x*8*300;
+		int y =9000*12;
+		int s = z-y;
+		System.out.println("一年賺的錢: "+z);
+		System.out.println("一年花了: "+y+" "+"一年存的錢: "+s);
+		
+	}
+}
